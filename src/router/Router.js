0ffb58630e634bf398f22055c6c67f3f38e5fb66
Hasan TOKER <tokerto7001@gmail.com>
2021-12-22 21:34:47 +0300

    navbar and router

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import Login from '../pages/Login';
 import Main from '../pages/Main';
 import Register from '../pages/Register';
@@ -7,7 +8,7 @@ const AppRouter = () => {
     
     return(
         <Router>
-
+        <Navbar />
         <Routes>
             <Route   path='/' element={<Main />} />
             <Route   path='/login' element={<Login />} />
