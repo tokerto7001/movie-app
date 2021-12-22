350ccae90a9bec7491823bb3be86829cef4a14fe
Hasan TOKER <tokerto7001@gmail.com>
2021-12-22 23:04:35 +0300

    last fixes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,13 @@
+import {useContext} from "react"
+import {AuthContext} from "../context/AuthContext"
 
 const IMG_API = 'https://image.tmdb.org/t/p/w1280'
 const defaultImage = "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
 
 
 const MovieCard = ({title, poster_path, overview, vote_average}) => {
+
+    const {currentUser} = useContext(AuthContext)
 
     const setVoteClass = (vote) => {
         if(vote >= 8){
@@ -20,7 +24,10 @@ const MovieCard = ({title, poster_path, overview, vote_average}) => {
             <img src={ poster_path ?  IMG_API + poster_path : defaultImage} alt={title}  />
             <div className='movie-info'>
             <h3>{title}</h3>
-            <span className={`tag ${setVoteClass(vote_average)}`}> {vote_average}</span>
+            {
+                currentUser ? <span className={`tag ${setVoteClass(vote_average)}`}> {vote_average}</span> : null
+            }
+            
             </div>
             <div className='movie-over'>
             <h2>Overview</h2>
